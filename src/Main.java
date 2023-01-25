de578d348b9b13ffd96017e5d2898505792ba8b7
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-25 19:26:25 +0400

    in the process1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Preparing key
+        String keyWord = "B2FutfZ3qHxhX9JJWZa0eUyqiGWJsIwL6FWmV32ggEFaJnBteoYZCYYWvxGHS929";
+        char[] key = new char[keyWord.length()];
+        for (int i = 0; i < keyWord.length(); i++) {
+            key [i] = keyWord.charAt(i);
+        }
+
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input: ");
+        String srcWord = s.next();
+
+        int count = 0;
+
+        for (int i = 0; i < ; i++) {
+
+        }
+        srcWord.replace();
+
+
     }
 }
