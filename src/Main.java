c07f9c0617981249d170d40e6c7547a1c945b683
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-26 13:59:14 +0400

    in the process3

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,22 @@ public class Main {
             key [i] = keyWord.charAt(i);
         }
 
-
-
         //Getting source word for encryption
         Scanner s = new Scanner(System.in);
         System.out.print("Input: ");
         String srcWord = s.next();
-
+        // взять Hex код символа ключа
+        //прибавить первую цифру к HEX коду символа слова
+        //получили новый код буквы
+        //обратно так же берем первую цифру hex кода ключа и вычитаем из зашифрованного символа hex кода
+        //получается hex код слова
         int count = 0;
         StringBuilder bldr = new StringBuilder(srcWord);
 
         for (int i = 0; i < srcWord.length() ; i++) {
             if (count == key.length) count = 0;
-
             int a = srcWord.charAt(i);
-            char
+            int b = key[i];
 
             bldr.setCharAt(i+a, key[count++]);
         }
