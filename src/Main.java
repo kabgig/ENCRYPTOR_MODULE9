717924c6cce93222c5b52be6807577f7c2ebebf0
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-26 17:58:18 +0400

    done without date/time 2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
+//REFACTOR AND ADD DATE AND TIME
 public class Main {
     public static void main(String[] args) {
-        //Preparing key
+        Scanner s = new Scanner(System.in);
+//Preparing key
         String keyWord = "B2FutfZ3qHxhX9JJWZa0eUyqiGWJsIwL6FWmV32ggEFaJnBteoYZCYYWvxGHS929";
         char[] key = new char[keyWord.length()];
         for (int i = 0; i < keyWord.length(); i++) {
             key[i] = keyWord.charAt(i);
         }
-
-        Scanner s = new Scanner(System.in);
-        //Encrypt or Decrypt
+//Encrypt or Decrypt
         System.out.println("1 - Encrypt");
         System.out.println("2 - Decrypt");
         String command = s.nextLine();
-
-        //Getting source word for encryption
+//Getting source word for encryption / decryption
         String srcWord = "";
         System.out.print("Input: ");
         srcWord = s.nextLine();
-        System.out.println(srcWord);
-
-        int count = 0;
         StringBuilder bldr = new StringBuilder(srcWord);
-        //Encrypting/Decrypting
+//Encrypting/Decrypting
         var res = Encrypt.encryption
-                (bldr, key, srcWord, count, command);
+                (bldr, key, srcWord, command);
         System.out.println(res);
 
 
